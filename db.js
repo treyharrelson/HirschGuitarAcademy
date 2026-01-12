7a197af33ad61f248415c03f9fb6da0d39f98916
mkenny904 <mlk040@email.latech.edu>
2026-01-12 13:45:08 -0600

    Should be working docker yaml

--- db.js
+++ db.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-    process.env.DB_DATABASE,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.POSTGRES_DB,
+    process.env.POSTGRES_USER,
+    process.env.POSTGRES_PASSWORD,
     {
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
