eff9ef9f8a331143d37e45d5e122d5ebcd948f65
mkenny904 <mlk040@email.latech.edu>
2026-02-21 12:11:39 -0600

    Ignore probably
    finicky, don't use

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono';
 import { db, schema } from '../db';
 import { eq } from 'drizzle-orm';
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 import { Session } from 'hono-sessions';
 
 const auth = new Hono<{ Variables: { session: Session } }>();
