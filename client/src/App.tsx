89d96ce2525ccbf0d7efaac92da982056bf574a4
JacobPace <127350081+JacobPace@users.noreply.github.com>
2026-04-03 09:03:12 -0500

    Remove unused code from App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,7 +12,6 @@ import MyCourses from './pages/MyCourses';
 // import Timer from './pages/student/metronome-tool/js/timer';
 import MainLayout from './pages/MainLayout';
 import './App.css';
-import Home from './pages/student/Home';
 import CourseDetails from './pages/student/CourseDetails';
 import MyEnrollments from './pages/student/MyEnrollments';
 import CourseView from './pages/student/CourseView';
@@ -22,11 +21,8 @@ import AddCourse from './pages/instructor/AddCourse'
 import InstructorMyCourses from './pages/instructor/MyCourses'
 import Instructor from './pages/instructor/Instructor'
 import InstrcutorDashboard from './pages/instructor/InstructorDashboard'
-import ProtectedRoute from './context/ProtectedRoute';
-
 
 import "quill/dist/quill.snow.css";
-import Navbar from './components/generic/Navbar';
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -41,9 +37,8 @@ function App() {
 
           {/** THIS IS THE MAIN RENDERED PAGE */}
           <Route element={<MainLayout />}>
-          
-          <Route path="/student-dashboard" element={<Dashboard />} />
-          <Route path="/admin-dashboard" element={<Dashboard />} />
+
+          <Route path="/home" element={<Dashboard />} />
           <Route path="/forum" element={<Forum />} />
           <Route path="/forum/thread/:threadId" element={<ThreadDetail />} />
           <Route path="/all-courses" element={<AllCourses />} />
@@ -52,7 +47,7 @@ function App() {
           <Route path="/metronome" element={<MyCourses />} />
           <Route path="/timer" element={<MyCourses />} />
 
-          <Route path='/home' element={<Home />} />
+          {/**<Route path='/home' element={<Home />} /> */}
           <Route path='/course-info/:id' element={<CourseDetails />} />
           <Route path='/my-enrollments' element={<MyEnrollments />} />
           <Route path='/course/:courseId' element={<CourseView />} />
