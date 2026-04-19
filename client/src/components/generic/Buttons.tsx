a580f24a65caafe1e5697c6c2b6e6fa7c8a3c6c5
mkenny904 <mlk040@email.latech.edu>
2026-04-19 13:43:22 -0500

    bigbluebutton can be disabled
    added optional disabled version of bigbluebutton

--- client/src/components/generic/Buttons.tsx
+++ client/src/components/generic/Buttons.tsx
@@ -3,12 +3,16 @@ import React from 'react'
 type ButtonProps = {
 	children: React.ReactNode;
 	onClick?: () => void;
+	disabled?: boolean;
 }
 
-export const BigBlueButton = ({ children, onClick }: ButtonProps) => (
+export const BigBlueButton = ({ children, onClick, disabled }: ButtonProps) => (
 	<button
-		onClick={onClick}
-		className='bg-blue-600 hover:bg-blue-700 text-white px-5 py-2 rounded-full transition-all'
+		onClick={disabled ? undefined : onClick}
+		disabled={disabled}
+		className={`px-5 py-2 rounded-full transition-all text-white ${
+			disabled ? 'bg-gray-400 cursor-not-allowed' : 'bg-blue-600 hover:bg-blue-700'
+		}`}
 	>
 		{children}
 	</button>
