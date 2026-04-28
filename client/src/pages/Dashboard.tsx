1e45150a40dde2d376ff17cded4799f63cb0adbd
mkenny904 <mlk040@email.latech.edu>
2026-04-28 16:53:18 -0500

    Sidebar more to left

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -133,7 +133,7 @@ function Dashboard() {
     }
 
     return (
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 bg-gray-50 min-h-screen">
+        <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8 py-8 bg-gray-50 min-h-screen">
             <div className="flex flex-col lg:flex-row gap-8">
 
                 <Sidebar>
