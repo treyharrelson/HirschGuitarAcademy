3724a36aa3c023e047c842f42d5c2b51bcc875fa
JacobPace <127350081+JacobPace@users.noreply.github.com>
2026-05-01 00:32:36 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -93,7 +93,7 @@ async function init() {
     console.log('DB connected');
 
     console.log('Starting sync...');
-    await sequelize.sync({ alter: false });
+    //await sequelize.sync({ alter: false });
     await sequelize.query('SET session_replication_role = DEFAULT;');
     console.log("Tables synced successfully.");
 
